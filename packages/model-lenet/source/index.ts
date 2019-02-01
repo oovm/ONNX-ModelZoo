e8db5b195ab6bacc905cbd042c80982942bce336
Aster <galaster@foxmail.com>
2019-02-01 14:09:15 +0800

    模型::Lenet test load

---
+++ packages/model-lenet/source/index.ts
@@ -0,0 +1,19 @@
+import { Tensor, InferenceSession } from 'onnxjs';
+
+export default async function main() {
+    // Create an ONNX inference session with WebAssembly backend.
+    const session = new InferenceSession({ backendHint: 'wasm' });
+
+    const url = 'https://github.com/GalAster/ONNX-ModelZoo/releases/download/v1.0.0/LeNet_trained_on_MNIST.onnx'
+    await session.loadModel(url);
+
+    const x = new Float32Array(1 * 1 * 28 * 28).fill(1);
+    const tensorX = new Tensor(x, 'float32', [1, 1, 28, 28]);
+
+    // Run model with Tensor inputs and get the result by output name defined in model.
+    const outputMap = await session.run([tensorX]);
+    const outputData = outputMap.get('sum');
+    console.log(outputData)
+}
+
+main();
