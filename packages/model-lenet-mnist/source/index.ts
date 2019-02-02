f010cdf71746fd19c82acf05a739b2f3c81c9191
Aster <galaster@foxmail.com>
2019-02-02 12:55:06 +0800

    项目::Name changed

--- packages/model-lenet-mnist/source/index.ts
+++ packages/model-lenet-mnist/source/index.ts
@@ -1,10 +1,19 @@
 import { Tensor, InferenceSession } from 'onnxjs';
 
+const urls = {
+    "Default": {
+        url: 'https://github.com/GalAster/ONNX-ModelZoo/releases/download/v1.0.0/LeNet_trained_on_MNIST.onnx',
+    },
+    "Lite": {
+        url: 'https://github.com/GalAster/ONNX-ModelZoo/releases/download/v1.0.0/LeNet_trained_on_MNIST.onnx',
+    }
+}
+
 export default async function main() {
     // Create an ONNX inference session with WebAssembly backend.
     const session = new InferenceSession({ backendHint: 'wasm' });
 
-    const url = 'https://github.com/GalAster/ONNX-ModelZoo/releases/download/v1.0.0/LeNet_trained_on_MNIST.onnx'
+    const url = urls.Default.url
     await session.loadModel(url);
 
     const x = new Float32Array(1 * 1 * 28 * 28).fill(1);
--- scripts/python/dynamic_onnx.py
+++ scripts/python/dynamic_onnx.py
@@ -4,7 +4,6 @@ from onnx import shape_inference
 
 def dynamic_onnx(path):
     original_model = onnx.load(path)
-    onnx.checker.check_model(original_model)
     # print('Before shape inference, the shape info of Y is:\n{}'.format(original_model.graph.value_info))
     original_model.graph.input[0].type.tensor_type.shape.dim[0].dim_param = 'batchsize'
     inferred_model = shape_inference.infer_shapes(original_model)
